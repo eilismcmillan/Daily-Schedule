812ce7c0769811c09097265c282b174ec35fd9d7
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-03-23 22:11:34 +1100

    added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,20 @@
 var saveButton = $(".saveBtn");
 
-
+// retrieved the current date from dayjs
 var presentDay = dayjs();
 $("#currentDay").text(presentDay.format("dddd, MMMM D"));
 
+// function to color-code the time slots
 var hourBlock = function () {
+  // retrieved current hour
   var currentHour = dayjs().hour();
 
+  // assigned the hour variable to the integer value of each time-block
   $(".time-block").each(function () {
     var hour = parseInt($(this).attr("id"));
 
-    console.log(hour);
+    // conditional statements comparing hour-slot and current hour
+    // applies class, and styling, to the relative time-slots
     if (hour < currentHour) {
       $(this).addClass("past");
     } else if (hour === currentHour) {
@@ -24,17 +28,25 @@ var hourBlock = function () {
   });
 };
 
+// event listener for click event
+// saving tasks to storage
 $(".saveBtn").on("click", function () {
+  // ensured that the relevant timeblock was matched with relevant hour time
   var timeBlock = $(this).closest(".time-block");
   var tasks = timeBlock.find(".description").val();
   var taskList = timeBlock.attr("id");
 
+  // set the task in local storage
+  // key for each task was the hour slot they were inputed in
   localStorage.setItem(taskList, tasks);
+  // message appears, then fades out each time save button is clicked
   var savedNotice = $("#saved-notice").text("Your task has been saved ✔️");
   savedNotice.show();
   setTimeout(savedNotice.fadeOut(1500));
 });
 
+// retrieves all saved items from storage
+// renders them onto the correct time slot
 var renderTasks = function () {
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
@@ -49,6 +61,3 @@ var renderTasks = function () {
 
 renderTasks();
 hourBlock();
-
-// TODO: Add ordinal to time
-// TODO: add coments
